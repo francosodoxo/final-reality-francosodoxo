8702eeeb9b2202e85040ba07f63a41326bc439c8
franco <franco.seguel@ug.uchile.cl>
2020-10-19 19:39:09 -0300

    weapon fixture created

--- src/test/java/com/github/cc3002/finalreality/model/equipweapon/TestEquipWeapon.java
+++ src/test/java/com/github/cc3002/finalreality/model/equipweapon/TestEquipWeapon.java
@@ -26,6 +26,12 @@ public class TestEquipWeapon {
   private String KNIGHT_NAME;
   private String THIEF_NAME;
 
+  private String AXE_NAME;
+  private String BOW_NAME;
+  private String KNIFE_NAME;
+  private String STAFF_NAME;
+  private String SWORD_NAME;
+
   private int HEALTH_POINTS;
   private int DEFENSE_POINTS;
   private int DAMAGE;
@@ -34,10 +40,33 @@ public class TestEquipWeapon {
   private BlockingQueue<IUnit> turnsQueue;
   @BeforeEach
   public void setUp(){
+    BLACK_MAGICIAN_NAME = "Javier";
+    WHITE_MAGICIAN_NAME = "Rodrigo";
+    ENGINEER_NAME = "Malu";
+    KNIGHT_NAME = "Quijote";
+    THIEF_NAME = "Andrea";
+
+    AXE_NAME = "Basic Axe";
+    BOW_NAME = "Basic Bow";
+    KNIFE_NAME = "Basic knife";
+    STAFF_NAME = "Basic Staff";
+    SWORD_NAME = "Basic Sword";
+
+    HEALTH_POINTS = 10;
+    DEFENSE_POINTS = 5;
+    DAMAGE = 3;
+    WEIGHT = 2;
+
     blackMagician = new BlackMagician(BLACK_MAGICIAN_NAME,turnsQueue,HEALTH_POINTS,DEFENSE_POINTS);
     whiteMagician = new WhiteMagician(WHITE_MAGICIAN_NAME,turnsQueue,HEALTH_POINTS,DEFENSE_POINTS);
     engineer = new Engineer(ENGINEER_NAME,turnsQueue,HEALTH_POINTS,DEFENSE_POINTS);
     knight = new Knight(KNIGHT_NAME,turnsQueue,HEALTH_POINTS,DEFENSE_POINTS);
     thief = new Thief(THIEF_NAME,turnsQueue,HEALTH_POINTS,DEFENSE_POINTS);
+
+    axe = new Axe(AXE_NAME,DAMAGE,WEIGHT);
+    bow = new Bow(BOW_NAME,DAMAGE,WEIGHT);
+    knife = new Knife(KNIFE_NAME,DAMAGE,WEIGHT);
+    staff = new Staff(STAFF_NAME,DAMAGE, WEIGHT);
+    sword = new Sword(SWORD_NAME,DAMAGE,WEIGHT);
   }
 }
